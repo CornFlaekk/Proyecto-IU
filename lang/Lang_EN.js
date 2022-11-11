a499800df41155b3eedc0fbc7c050a8a7b8df9ab
CornFlaekk <roy062002@gmail.com>
2022-11-11 22:33:03 +0100

    Traducción EN

--- lang/Lang_EN.js
+++ lang/Lang_EN.js
@@ -1,107 +1,312 @@
 arrayEN = {
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////
-	/////////////////////////////////////////PRACTICA 4////////////////////////////////////////////////
+	/////////////////////////////////////////acciones///////////////////////////////////////////////////
 	////////////////////////////////////////////////////////////////////////////////////////////////////
-	//Codigos PRACTICA 4
-	'pagina_usuario_wellcome': 'User Management',
-	'dni': 'DNI',
-	'usuario': 'User Login',
-	'id_rol': 'User Role',
 
-	////////////////////////////////////////////////////////////////////////////////////////////////////
-	/////////////////////////////////////////PRACTICA 5////////////////////////////////////////////////
-	////////////////////////////////////////////////////////////////////////////////////////////////////
-	//Codigos PRACTICA 5
-	'pagina_persona_wellcome': 'People Management',
-	'dni': 'DNI',
-	'nombre_persona': 'Name',
-	'apellidos_persona': 'Surname',
-	'fechaNacimiento_persona': 'Birth Date',
-	'direccion_persona': 'Adress',
-	'telefono_persona': 'Telephone',
-	'email_persona': 'Email',
-	'foto_persona': 'Photo',
-
-	'boton_enviar': 'Send',
-
-	////////////////////////////////////////////////////////////////////////////////////////////////////
-	/////////////////////////////////////////acciones////////////////////////////////////////////////
-	////////////////////////////////////////////////////////////////////////////////////////////////////
 	//Codigos acciones
 	'titulo_edit': 'Edit',
 	'titulo_delete': 'Delete',
 	'titulo_showcurrent': 'Detail',
+	'titulo_volver': 'Return',
+	'titulo_refersh': 'Refresh',
+
+	'boton_enviar': 'Send',
 
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////
-	/////////////////////////////////////////Errores FRONT//////////////////////////////////////////////
+	/////////////////////////////////////////Códigos FRONT//////////////////////////////////////////////
 	////////////////////////////////////////////////////////////////////////////////////////////////////
-	//Códigos
-	'usuario_corto_ko': 'Login size too short (min 3 characters)',
-	'usuario_largo_ko': 'Login size too long (max 15 characters)',
-	'usuario_formato_ko': 'Login contains not allowed characters (only letter without accent and numbers are allowed)',
+
+	/////////////////////////////////////////INDEX//////////////////////////////////////////////////////
+
+
+	'titulo_index': 'Index',
+
+
+	/////////////////////////////////////////LOGIN//////////////////////////////////////////////////////
+
+
+	'titulo_login': 'Login',
+
+
+	/////////////////////////////////////////REGISTRO///////////////////////////////////////////////////
+
+
+	'titulo_registro': 'Register',
+
+
+	/////////////////////////////////////////CONTRASEÑA/////////////////////////////////////////////////
+
+
+	'titulo_contrasena': 'Change Password',
+
+
+	/////////////////////////////////////////////MENU///////////////////////////////////////////////////
+
+
+	'titulo_menu': 'Menu',
+	'paginas_disponibles': 'Available pages:',
+
+
+	/////////////////////////////////////////USUARIO////////////////////////////////////////////////////
+
+
+	'titulo_usuario': 'User Management',
+
+	'usuario': 'User Login ',
+	'contrasena': 'Password',
+	'contrasena_repetir': 'Repeat password',
+
+	'usuario_corto_ko': 'Login size too short (Minimum 3 characters)',
+	'usuario_largo_ko': 'Login size too long (Maximum 45 characters)',
+	'usuario_formato_ko': 'Login contains characters not allowed (only letters without accents and numbers)',
+
+	'contrasena_corto_ko': 'Password is too short (Minimum 3 characters)',
+	'contrasena_largo_ko': 'The password is too long (Maximum 45 characters)',
+	'contrasena_formato_ko': 'The password contains invalid characters (Alphabet characters without accents, numbers, "-" and "_" are allowed")',
+
+	'contrasena_igual_ko': 'Passwords do not match',
+
+
+	/////////////////////////////////////////PERSONA////////////////////////////////////////////////////
+	
+	
+	'titulo_persona': 'People Management',
+
+	'dni': 'DNI',
+	'nombre_persona': 'Name',
+	'apellidos_persona': 'Surname',
+	'fechaNacimiento_persona': 'Birth Date',
+	'direccion_persona': 'Address',
+	'telefono_persona': 'Telephone',
+	'email_persona': 'Email',
+	'foto_persona': 'Photo',
+
+	//dni
+	'dni_corto_ko': 'DNI size too short (8 Numbers and 1 Letter)',
+	'dni_largo_ko': 'DNI size too short (8 Numbers and 1 Letter)',
+	'dni_formato_ko': 'Wrong DNI format (8 Numbers and 1 Letter)',
+	'dni_letra_ko': 'The letter of the ID does not correspond to the numbers',
+	'dni_formato_search_ko': 'The value for DNI search is not correct',
+
+	//nombre
+	'nombre_persona_corto_ko': 'Name size too short (min 3 characters)',
+	'nombre_persona_largo_ko': 'Name size too long (max 45 characters)',
+	'nombre_persona_formato_ko': 'The name contains illegal characters (only letters, spaces and hyphens (-)',
+
+	//apellido
+	'apellidos_persona_corto_ko': 'Surname size too short (min 5 characters)',
+	'apellidos_persona_largo_ko': 'Surname size too long (max 100 characters)',
+	'apellidos_persona_formato_ko': 'The surnames contain characters not allowed (only letters, spaces and hyphens (-)',
+
+	//fechaNacimiento
+
+	'fechaNacimiento_persona_corto_ko': 'Date size too short (min 10 characters)',
+	'fechaNacimiento_persona_largo_ko': 'Date size too long (max 10 characters)',
+	'fechaNacimiento_persona_formato_ko': 'The date contains illegal characters (DD/MM/YYYY)',
+
+	//dirección
+	'direccion_persona_corto_ko': 'Address size too short (min 10 characters)',
+	'direccion_persona_largo_ko': 'Address size too long (max 200 characters)',
+	'direccion_persona_formato_ko': 'The address includes non-permitted characters (Alphabet characters with accents, numbers, spaces, "/", "-", ",", "º" and "ª" are allowed)',
+
+	//telefono
+	'telefono_persona_corto_ko': 'Phone size too short (min 9 characters)',
+	'telefono_persona_largo_ko': 'Phone size too long (max 9 characters)',
+	'telefono_persona_formato_ko': 'The phone contains illegal characters (only numbers)',
+	'telefono_persona_espana_ko': 'The telephone does not have a Spanish format (It starts with 6,7,8 or 9)',
+
+
+	//email
+	'email_persona_corto_ko': 'Email size too short (min 8 characters)',
+	'email_persona_largo_ko': 'Email size too long (max 45 characters)',
+	'email_persona_caracteres_ko': 'The email contains characters not allowed (Allowed: lowercase letters without accent, numbers, hyphens ("-" or "_") periods, apostrophes (‘) and "@")',
+
+	'email_persona_formato_ko': 'The email does not have the correct format ( example@domain.extension )',
+
+	//foto
+	'foto_persona_corto_ko': 'Photo size too short (min 6 characters)',
+	'foto_persona_largo_ko': 'Photo size too long (max 40 characters)',
+	'foto_persona_caracteres_ko': 'The photo contains characters not allowed (only letters without accents and ".")',
+	'foto_persona_formato_ko': 'The photo contains characters not allowed (only letters without accents and ".")',
+
+
+	/////////////////////////////////////////////ROL////////////////////////////////////////////////////
+
+
+	'titulo_rol': 'Role Management',
+
+	'id_rol': 'identifier',
+	'nombre_rol': 'Name',
+	'descrip_rol': 'Description',
+
+	//id_rol
+	'id_rol_largo_ko': 'The role id field cannot have more than 5 numbers',
+	'id_rol_formato_ko': 'The role id field must be made up of numbers',
+
+	//nombre
+	'nombre_rol_corto_ko': 'The role name field cannot have less than 6 characters',
+	'nombre_rol_largo_ko': 'The role name field cannot have more than 48 characters',
+	'nombre_rol_formato_ko': 'The role name field must be made up of alphabetic characters without an accent',
+
+	//descrip
+	'descrip_rol_corto_ko': 'Role description must be at least 20 characters',
+	'descrip_rol_largo_ko': 'The role description must have a maximum of 200 characters',
+	'descrip_rol_caracteres_ko': 'The role description cannot contain the following characters: =<>$#{}[]',
+
+
+	/////////////////////////////////////////////ACCION/////////////////////////////////////////////////
+
+
+	'titulo_accion': 'Actions Management',
+
+	'id_accion': 'Identifier',
+	'nombre_accion': 'Name',
+	'descrip_accion': 'Description',
+
+
+	//id_accion
+	'id_accion_largo_ko': 'The action id field cannot have more than 5 numbers',
+	'id_accion_formato_ko': 'The action id field must be made up of numbers',
+
+	//nombre
+	'nombre_accion_corto_ko': 'The action name field cannot have less than 6 characters',
+	'nombre_accion_largo_ko': 'The action name field cannot have more than 48 characters',
+	'nombre_accion_formato_ko': 'The action name field must be made up of alphabetic characters without an accent',
+
+	//descrip
+	'descrip_accion_corto_ko': 'Action description must have at least 20 characters',
+	'descrip_accion_largo_ko': 'Action description must have a maximum of 200 characters',
+	'descrip_accion_caracteres_ko': 'Action description cannot contain the following characters: =<>$#{}[]',
+
+
+	/////////////////////////////////////////////FUNCIONALIDAD////////////////////////////////////////////
+
+	'titulo_funcionalidad': 'Functionalities Management',
+
+	'id_funcionalidad': 'Identifier',
+	'nombre_funcionalidad': 'Name',
+	'descrip_funcionalidad': 'Description',
+
+	//id_funcionalidad
+	'id_funcionalidad_largo_ko': 'The functionality id field cannot have more than 5 numbers',
+	'id_funcionalidad_formato_ko': 'The functionality id field must be made up of numbers',
+
+	//nombre
+	'nombre_funcionalidad_corto_ko': 'The functionality name field cannot have less than 6 characters',
+	'nombre_funcionalidad_largo_ko': 'The functionality name field cannot have more than 48 characters',
+	'nombre_funcionalidad_formato_ko': 'The functionality name field must be made up of alphabetic characters without an accent',
+
+	//descrip
+	'descrip_funcionalidad_corto_ko': 'Functionality description must have at least 20 characters',
+	'descrip_funcionalidad_largo_ko': 'Functionality description must have a maximum of 200 characters',
+	'descrip_funcionalidad_caracteres_ko': 'Functionality description cannot contain the following characters: =<>$#{}[]',
+
+
+	///////////////////////////////////ROLACCIONFUNCIONALIDAD////////////////////////////////////////////
+
+
+	'titulo_rolaccionfuncionalidad': 'Role-Action-Functionality Management',
+	'rol': 'Role',
+	'funcionalidad': 'Functionality',
+	'accion': 'Action',
+
+
 	////////////////////////////////////////////////////////////////////////////////////////////////////
 	/////////////////////////////////////////Errores BACK///////////////////////////////////////////////
 	////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 	//generales
-	'SQL_OK': 'SQL_OK',
-	'SQL_KO': 'SQL_KO',
-	'CONEXION_KO': 'CONEXION_KO',
-	'RECORDSET_VACIO': 'RECORDSET_VACIO',
-	'RECORDSET_DATOS': 'RECORDSET_DATOS',
+	'SQL_OK': 'Request successful',
+	'SQL_KO': 'Request has been wrong',
+	'CONEXION_KO': 'Connection dropped',
+	'RECORDSET_VACIO': 'Empty received data',
+	'RECORDSET_DATOS': 'Data received successfully',
 
 	// registrar
-	'REGISTRAR_OK': 'REGISTRAR_OK',
-	'CAMBIAR_contrasena_KO': 'CAMBIAR_contrasena_KO',
-	'CAMBIAR_contrasena_OK': 'CAMBIAR_contrasena_OK',
+	'REGISTRAR_OK': 'Registration successful',
+	'CAMBIAR_contrasena_KO': 'Password change error',
+	'CAMBIAR_contrasena_OK': 'Password change successful',
 
 	// login
-	'LOGIN_OK': 'LOGIN_OK',
-	'USUARIO_PASS_KO': 'USUARIO_PASS_KO',
-	'USUARIO_LOGIN_KO': 'USUARIO_LOGIN_KO',
+	'LOGIN_OK': 'Successful login',
+	'USUARIO_PASS_KO': 'Wrong password',
+	'USUARIO_LOGIN_KO': 'User does not exist',
 
 	// persona
-	'dni_EXISTE_en_persona_KO': 'dni_EXISTE_en_persona_KO',
-	'dni_NO_EXISTE_en_persona_KO': 'dni_NO_EXISTE_en_persona_KO',
+	'dni_EXISTE_en_persona_KO': 'DNI already exists in the person table',
+	'dni_NO_EXISTE_en_persona_KO': 'DNI does not exist in the person table',
 
-	'dni_EXISTE_en_usuario_KO': 'dni_EXISTE_EN_usuario_KO',
-	'dni_NO_EXISTE_en_usuario_KO': 'dni_NO_EXISTE_en_usuario_KO',
+	'dni_EXISTE_EN_usuario_KO': 'DNI already exists in the user table',
+	'dni_NO_EXISTE_en_usuario_KO': 'DNI does not exist in the user table',
 
-	'email_EXISTE_en_persona_KO': 'email_EXISTE_en_persona_KO',
+	'email_EXISTE_en_persona_KO': 'Email already exists in the person table',
+
+	'add_persona_OK': 'Person added successfully',
+	'edit_persona_OK': 'Person successfully edited',
+	'delete_persona_OK': 'Person successfully deleted',
+	'search_persona_OK': 'Search for people successful',
 
 	// usuario
-	'usuario_EXISTE_en_usuario_KO': 'usuario_EXISTE_EN_usuario_KO',
-	'admin_no_se_puede_modificar_KO': 'admin_no_se_puede_modificar_KO',
-	'admin_no_se_puede_borrar_KO': 'admin_no_se_puede_borrar_KO',
+	'usuario_EXISTE_en_usuario_KO': 'User already exists in the user table',
+	'admin_no_se_puede_modificar_KO': 'Admin cannot be modified',
+	'admin_no_se_puede_borrar_KO': 'Admin cannot be deleted',
+
+	'add_usuario_OK': 'User added successfully',
+	'edit_usuario_OK': 'User edited successfully',
+	'delete_usuario_OK': 'User deleted successfully',
+	'search_usuario_OK': 'User search successful',
 
 	// rol
-	'id_rol_EXISTE_en_usuario_KO': 'id_rol_EXISTE_en_usuario_KO',
-	'no_puede_borrar_rol_adminybasico': 'no_puede_borrar_rol_adminybasico',
-	'no_puede_editar_rol_adminybasico': 'no_puede_editar_rol_adminybasico',
+	'id_rol_EXISTE_en_usuario_KO': 'Rol already exists in the role table',
+	'no_puede_borrar_rol_adminybasico': 'Role Admin and Basic cannot be deleted',
+	'no_puede_editar_rol_adminybasico': 'Role Admin and Basic cannot be modified',
 
-	'id_rol_EXISTE_en_rol_KO': 'id_rol_EXISTE_en_rol_KO',
-	'id_rol_NO_EXISTE_en_rol_KO': 'id_rol_NO_EXISTE_en_rol_KO',
+	'id_rol_EXISTE_en_rol_KO': 'Rol already exists in the role table',
+	'id_rol_NO_EXISTE_en_rol_KO': 'Rol does not exist in the role table',
 
-	'id_rol_EXISTE_en_rolaccionfuncionalidad_KO': 'id_rol_EXISTE_en_rolaccionfuncionalidad_KO',
+	'id_rol_EXISTE_en_rolaccionfuncionalidad_KO': 'Rol exists in the role-action-functionality table',
+
+	'add_rol_OK': 'Role added successfully',
+	'edit_rol_OK': 'Role edited successfully',
+	'delete_rol_OK': 'Role deleted successfully',
+	'search_rol_OK': 'Role search successful',
 
 	// accion
-	'id_accion_EXISTE_en_accion_KO': 'id_accion_EXISTE_en_accion_KO',
-	'id_accion_NO_EXISTE_en_accion_KO': 'id_accion_NO_EXISTE_en_accion_KO',
-	'id_accion_EXISTE_en_rolaccionfuncionalidad_KO': 'id_accion_EXISTE_en_rolaccionfuncionalidad_KO',
-	'no_puede_editar_acciones_admin': 'no_puede_editar_acciones_admin',
+	'id_accion_EXISTE_en_accion_KO': 'Action already exists in the action table',
+	'id_accion_NO_EXISTE_en_accion_KO': 'Action does not exist in the action table',
+	'id_accion_EXISTE_en_rolaccionfuncionalidad_KO': 'Action exists in the role-action-functionality table',
+	'no_puede_editar_acciones_admin': 'Admin action cannot be modified',
+
+	'add_accion_OK': 'Action added successfully',
+	'edit_accion_OK': 'Action edited successfully',
+	'delete_accion_OK': 'Action deleted successfully',
+	'search_accion_OK': 'Search for action performed successfully',
 
 	// funcionalidad
-	'id_funcionalidad_EXISTE_en_funcionalidad_KO': 'id_funcionalidad_EXISTE_en_funcionalidad_KO',
-	'id_funcionalidad_NO_EXISTE_en_funcionalidad_KO': 'id_funcionalidad_NO_EXISTE_en_funcionalidad_KO',
-	'id_funcionalidad_EXISTE_en_rolaccionfuncionalidad_KO': 'id_funcionalidad_EXISTE_en_rolaccionfuncionalidad_KO',
-	'no_puede_editar_funcionalidades_admin': 'no_puede_editar_funcionalidades_admin',
-	'no_puede_borrar_funcionalidades_admin': 'no_puede_borrar_funcionalidades_admin',
+	'id_funcionalidad_EXISTE_en_funcionalidad_KO': 'Functionality already exists in the functionality table',
+	'id_funcionalidad_NO_EXISTE_en_funcionalidad_KO': 'Functionality does not exist in the functionality table',
+	'id_funcionalidad_EXISTE_en_rolaccionfuncionalidad_KO': 'Functionality exists in the role-action-functionality table',
+	'no_puede_editar_funcionalidades_admin': 'Admin functionality cannot be modified',
+	'no_puede_borrar_funcionalidades_admin': 'Admin functionality cannot be removed',
+
+	'add_funcionalidad_OK': 'Functionality added successfully',
+	'edit_funcionalidad_OK': 'Functionality edited successfully',
+	'delete_funcionalidad_OK': 'Functionality removed successfully',
+	'search_funcionalidad_OK': 'Feature search successful',
 
 	// rolaccionfuncionalidad
-	'no_puede_borrar_permiso_admin': 'no_puede_borrar_permiso_admin',
-	'no_puede_editar_permiso_admin': 'no_puede_editar_permiso_admin',
-	'permiso_EXISTE_en_rolaccionfuncionalidad_KO': 'permiso_EXISTE_en_rolaccionfuncionalidad_KO',
-	'permiso_NO_EXISTE_en_rolaccionfuncionalidad_KO': 'permiso_NO_EXISTE_en_rolaccionfuncionalidad_KO',
-	'prohibido_edit_rolaccionfuncionalidad': 'prohibido_edit_rolaccionfuncionalidad'
+	'no_puede_borrar_permiso_admin': 'Admin permission cannot be removed',
+	'no_puede_editar_permiso_admin': 'Admin permission cannot be modified',
+	'permiso_EXISTE_en_rolaccionfuncionalidad_KO': 'Permission already exists in role-action-functionality',
+	'permiso_NO_EXISTE_en_rolaccionfuncionalidad_KO': 'Permission does not exist in role-action-functionality',
+	'prohibido_edit_rolaccionfuncionalidad': 'Forbidden to edit permissions in role-action-functionality',
+
+	'add_rolaccionfuncionalidad_OK': 'Permission added successfully',
+	'edit_rolaccionfuncionalidad_OK': 'Permission edited successfully',
+	'delete_rolaccionfuncionalidad_OK': 'Permission removed successfully',
+	'search_rolaccionfuncionalidad_OK': 'Permission search successful',
+
 }
